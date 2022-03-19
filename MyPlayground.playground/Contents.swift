e6bc74ce54f9368229439380855351f2907af8b5
Aldhafiri70 <thelordshoes@Gmail.com>
2022-03-19 19:17:25 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct student{
+    let name: String
+    let age: Int
+    let grade: Double
+    
+}
+let Rezo = student(name:"Abdulrazaq", age: 15,grade: 98.5)
+let Friend=student(name: "Kaled", age: 15, grade: 95.2)
+
+print(Rezo.grade)
+print(Friend.grade)
