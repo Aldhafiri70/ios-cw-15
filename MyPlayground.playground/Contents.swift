fee477ed57c0954f541542d49aa01fae3599d889
Aldhafiri70 <thelordshoes@Gmail.com>
2022-03-19 20:02:53 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,10 +4,10 @@ struct student{
     let name: String
     let age: Int
     let grade: Double
-    
+    var attendess: Bool
 }
-let Rezo = student(name:"Abdulrazaq", age: 15,grade: 98.5)
-let Friend=student(name: "Kaled", age: 15, grade: 95.2)
+let Rezo = student(name:"Abdulrazaq", age: 15,grade: 98.5,attendess:true )
+let Friend=student(name: "Kaled", age: 15, grade: 95.2,attendess:false )
 
 print(Rezo.grade)
 print(Friend.grade)
